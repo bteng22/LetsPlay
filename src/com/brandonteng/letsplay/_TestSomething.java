71ccedf94ef081d1b4991f7c98af52eb9c70d067
Brandon Teng <bteng22@gmail.com>
2014-07-02 07:02:38 +0530

    first commit

---
+++ src/com/brandonteng/letsplay/_TestSomething.java
@@ -0,0 +1,33 @@
+package com.brandonteng.letsplay;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by brandonteng on 7/2/14.
+ */
+public class _TestSomething {
+
+    @Test
+    public void depositAndWithdrawal() {
+        SavingsAccount account = new SavingsAccount();
+        account.deposit(100);
+
+        //Comparing two ways two assert
+        assertThat("after deposit", account.balance(), is(100));
+       // assertEquals(100, account.balance());
+        account.withdraw(50);
+        assertThat("after withdrawal", account.balance(), is(50));
+    }
+    
+    @Test
+    public void negativeBalanceIsJustFine() {
+        SavingsAccount account = new SavingsAccount();
+        account.withdraw(75);
+        assertThat(account.balance(), is(-75));
+    }
+
+
+}
